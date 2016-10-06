2a8734b2dde534e6b5b75a68ff383f2261017cb1
Lucas Ferreira <lucas.ferreira@fpf.br>
2016-10-06 03:40:01 -0400

    Testando navegação entre telas.

--- lcMarket-frontend/src/app/index.module.js
+++ lcMarket-frontend/src/app/index.module.js
@@ -1,7 +1,10 @@
 (function() {
-  'use strict';
+	'use strict';
+	angular
+	.module('lcMarketFrontend', ['ngCookies', 'ngSanitize', 'ngMessages', 'ngResource', 'ngRoute', 'ngMaterial', 'ngMdIcons'])
+	.controller('IndexController', IndexController);
 
-  angular
-    .module('lcMarketFrontend', ['ngCookies', 'ngSanitize', 'ngMessages', 'ngResource', 'ngRoute', 'ngMaterial', 'ngMdIcons']);
-
+	function IndexController($rootScope) {		
+		$rootScope.currentCart;
+	}
 })();
