004770cdc54c6c1f413f47d23dc85696a9dd77e2
lc.asferreira <lc.asferreira001@gmail.com>
2016-10-06 13:43:09 -0400

    Adicionado método para pesquisar carrinho pelo status

--- lcMarket/src/main/java/com/lcferreira/market/repositories/ShoppingCartRepository.java
+++ lcMarket/src/main/java/com/lcferreira/market/repositories/ShoppingCartRepository.java
@@ -1,10 +1,19 @@
 package com.lcferreira.market.repositories;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.lcferreira.market.model.ShoppingCart;
+import com.lcferreira.market.model.ShoppingCartStatus;
 
 public interface ShoppingCartRepository extends CrudRepository<ShoppingCart, Long>{
 
-	
+	/**
+	 * Method to return the open shopping cart (BUYING), if exists login, 
+	 * this method should be changed to be used for each user.
+	 * 
+	 * @return
+	 */
+	@Query("select sc from ShoppingCart sc where sc.status = ?")
+	public ShoppingCart getShoppingCartByStatus(ShoppingCartStatus status);
 }
