4d5d354b6e1159126dab6c8a1456000095d34ae2
Lucas Ferreira <lucas.ferreira@fpf.br>
2016-10-06 03:38:53 -0400

    Adicionando estrutura para página de carrinho de compras

---
+++ lcMarket-frontend/src/app/shopping_cart/shopping_cart.controller.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('lcMarketFrontend')
+    .controller('ShoppingCartController', ShoppingCartController);
+
+  /** @ngInject */
+  function ShoppingCartController($routeParams, ShoppingCartService) {
+
+    var vm = this;
+
+    vm.shoppingCart;
+
+    vm.idShoppingCart = $routeParams.id;
+
+    alert(vm.idShoppingCart);
+
+    if(vm.idShoppingCart){
+      ShoppingCartService.getShoppingCart(vm.idShoppingCart).then(function(response) {
+          vm.shoppingCart = angular.copy(response);
+      });  
+    }
+
+    vm.removeProduct = function(idProduct){
+        ShoppingCartService.removeProductFromCart(idProduct, vm.idShoppingCart).then(function(response){
+            vm.shoppingCart = angular.copy(response);
+        }); 
+    }
+  }
+})();
--- lcMarket-frontend/src/app/shopping_cart/shopping_cart.service.js
+++ lcMarket-frontend/src/app/shopping_cart/shopping_cart.service.js
@@ -7,10 +7,9 @@
 
   /** @ngInject */
   function ShoppingCartService($http, $q) {
-    
+  
     this.addProduct = function(idProduct){
       var deferred = $q.defer();
-      //var params = ConfigurationService.getAsUriParam(filter);
       var url = 'http://localhost:8680/lcmarket/shoppingKarts/products/' + idProduct;
         $http.post(url)
       .then(function(response){
@@ -23,6 +22,48 @@
       return deferred.promise;
     }
 
+    this.addProductToCart = function(idProduct, idShoppingCart){
+      var deferred = $q.defer();
+      var url = 'http://localhost:8680/lcmarket/shoppingKarts/' + idShoppingCart + '/products/' + idProduct;
+        $http.put(url)
+      .then(function(response){
+        deferred.resolve(response.data);
+        return deferred.promise;
+      },  function (response) {
+        deferred.reject(response);
+        return deferred.promise;
+      });
+      return deferred.promise;
+    }
+
+    this.removeProductFromCart = function(idProduct, idShoppingCart){
+      var deferred = $q.defer();
+      var url = 'http://localhost:8680/lcmarket/shoppingKarts/' + idShoppingCart + '/products/' + idProduct;
+        $http.delete(url)
+      .then(function(response){
+        deferred.resolve(response.data);
+        return deferred.promise;
+      },  function (response) {
+        deferred.reject(response);
+        return deferred.promise;
+      });
+      return deferred.promise;
+    }
+
+    this.getShoppingCart = function(idShoppingCart){
+      var deferred = $q.defer();
+      var url = 'http://localhost:8680/lcmarket/shoppingKarts/' + idShoppingCart;
+        $http.get(url)
+      .then(function(response){
+        deferred.resolve(response.data);
+        return deferred.promise;
+      },  function (response) {
+        deferred.reject(response);
+        return deferred.promise;
+      });
+      return deferred.promise;
+    }
+  
 
     //ScrapService.$inject = ['$http', '$q'];
     
